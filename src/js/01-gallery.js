d0d4ffa9a5feee4db718f90d0176f48e8f56c509
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-10 18:21:03 +0300

    import css

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log("Hello!");
+// Імпорт бібліотеки SimpleLightbox
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => 
+                 `<li class="gallery__item">
+                 <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                 </a>
+              </li>
+                 `
+).join('');
+galleryList.insertAdjacentHTML('beforeend', markup);
+
 console.log(galleryItems);
