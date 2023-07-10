c2ff531495fd7caaea43de87868faa50cbb9a868
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-10 22:09:10 +0300

    gallery1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,16 +8,14 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryList = document.querySelector('.gallery');
 const markup = galleryItems.map(({preview, original, description}) => 
-                 `<li class="gallery__item">
-                 <a class="gallery__link" href="${original}">
+                 `<a class="gallery__link" href="${original}">
                     <img class="gallery__image" src="${preview}" alt="${description}" />
                  </a>
-              </li>
                  `
 ).join('');
 galleryList.insertAdjacentHTML('beforeend', markup);
 
-const lightbox = new SimpleLightbox('.gallery a', { 
+new SimpleLightbox('.gallery a', { 
    captionsData: 'alt',
    captionDelay: 250
 });
