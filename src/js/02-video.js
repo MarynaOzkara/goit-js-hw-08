591dd5ecfca8d0ae726f94587d621851b184d5b2
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-12 14:32:40 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = 'videoplayer-current-time';
+
+const onPlay = function(data) {
+    localStorage.getItem(currentTime, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(currentTime)) || 0);
+
+
 
