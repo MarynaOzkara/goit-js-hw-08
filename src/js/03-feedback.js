d1ce73efdad06d286aaf19238ed775c73634be03
MarynaOzkara <ozkaramaryna@gmail.com>
2023-07-13 10:54:17 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,33 @@
 import throttle from "lodash.throttle";
 
-const STORAGE_KEY = 'feedback-form-state';
+const DATA_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+
+let dataForm = JSON.parse(localStorage.getItem(DATA_KEY)) || {};
+const {email, message} = form.elements;
+ 
+
+function onInput() {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(DATA_KEY, JSON.stringify(dataForm));
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const {email, message} = event.currentTarget.elements;
+  if(email.value === "" || message.value === "") {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    dataForm = { email: email.value, message: message.value }
+    console.log(dataForm);
+  }
+  localStorage.removeItem(DATA_KEY);
+  form.reset();
+  dataForm = {};
+}
+
